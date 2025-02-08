ca0a367e4434b9c75fb88880891736f83f46d703
nlamprea <nlamprea@unal.edu.co>
2025-02-07 20:16:02 -0500

    tall

---
+++ manet.cc
@@ -0,0 +1,185 @@
+// incompleto
+#include "ns3/aodv-module.h"
+#include "ns3/applications-module.h"
+#include "ns3/core-module.h"
+#include "ns3/dsdv-module.h"
+#include "ns3/dsr-module.h"
+#include "ns3/flow-monitor-module.h"
+#include "ns3/internet-module.h"
+#include "ns3/mobility-module.h"
+#include "ns3/network-module.h"
+#include "ns3/olsr-module.h"
+#include "ns3/yans-wifi-helper.h"
+
+#include <fstream>
+#include <iostream>
+
+
+using namespace ns3;
+using namespace dsr;
+
+NS_LOG_COMPONENT_DEFINE("MANETRouting");
+
+
+// void ReceivePacket(Ptr<Socket> socket)
+// {
+//     while (socket->Recv())
+//     {
+//         NS_LOG_UNCOND("Received one packet!");
+//     }
+// }
+// Funciones de callback para los traces de IPv4
+static void Ipv4TxTrace(Ptr<const Packet> packet, Ptr<Ipv4> ipv4, uint32_t interface)
+{
+    Ipv4Header ipHeader;
+    packet->PeekHeader(ipHeader);
+    NS_LOG_INFO("Enviando paquete desde " << ipHeader.GetSource() << " hacia " << ipHeader.GetDestination());
+}
+
+static void Ipv4RxTrace(Ptr<const Packet> packet, Ptr<Ipv4> ipv4, uint32_t interface)
+{
+    Ipv4Header ipHeader;
+    packet->PeekHeader(ipHeader);
+    Ipv4InterfaceAddress localAddress = ipv4->GetAddress(interface, 0);
+    NS_LOG_INFO("Recibiendo paquete en " << localAddress.GetLocal() << " desde " << ipHeader.GetSource() << " hacia " << ipHeader.GetDestination());
+}
+
+int main(int argc, char* argv[])
+{
+    int nodesWifi1 = 10; // Number of nodes
+    int nodesWifi2 = 10; // Number of nodes
+    double totalTime = 100.0; // Total simulation time
+    std::string rate("2048bps");
+    std::string phyMode("DsssRate11Mbps");
+    std::string tr_name("manet-routing-compare");
+    int nodeSpeed = 20; // in m/s
+    int nodePause = 0;  // in s
+
+    Config::SetDefault("ns3::OnOffApplication::PacketSize", StringValue("64"));
+    Config::SetDefault("ns3::OnOffApplication::DataRate", StringValue(rate));
+
+    // Set Non-unicastMode rate to unicast mode
+    Config::SetDefault("ns3::WifiRemoteStationManager::NonUnicastMode", StringValue(phyMode));
+
+
+    NodeContainer p2pNodes;
+    p2pNodes.Create(2);
+
+    PointToPointHelper pointToPoint;
+    pointToPoint.SetDeviceAttribute("DataRate", StringValue("5Mbps"));
+    pointToPoint.SetChannelAttribute("Delay", StringValue("2ms"));
+
+    NodeContainer adhocNodes1;
+    adhocNodes1.Create(nodesWifi1);
+
+    NodeContainer adhocNodes2;
+    adhocNodes2.Create(nodesWifi2);
+    
+    // setting up wifi phy and channel using helpers
+    WifiHelper wifi;
+    wifi.SetStandard(WIFI_STANDARD_80211b);
+
+    YansWifiPhyHelper wifiPhy;
+    YansWifiChannelHelper wifiChannel;
+    wifiChannel.SetPropagationDelay("ns3::ConstantSpeedPropagationDelayModel");
+    wifiChannel.AddPropagationLoss("ns3::FriisPropagationLossModel");
+    wifiPhy.SetChannel(wifiChannel.Create());
+
+    // Add a mac and disable rate control
+    WifiMacHelper wifiMac;
+    wifi.SetRemoteStationManager("ns3::ConstantRateWifiManager",
+                                 "DataMode",
+                                 StringValue(phyMode),
+                                 "ControlMode",
+                                 StringValue(phyMode));
+
+    wifiPhy.Set("TxPowerStart", DoubleValue(m_txp));
+    wifiPhy.Set("TxPowerEnd", DoubleValue(m_txp));
+
+    wifiMac.SetType("ns3::AdhocWifiMac");
+    NetDeviceContainer adhocDevices1 = wifi.Install(wifiPhy, wifiMac, adhocNodes1);
+    NetDeviceContainer adhocDevices2 = wifi.Install(wifiPhy, wifiMac, adhocNodes2);
+
+    MobilityHelper mobilityAdhoc;
+    int64_t streamIndex = 0; // used to get consistent mobility across scenarios
+
+    ObjectFactory pos;
+    pos.SetTypeId("ns3::RandomRectanglePositionAllocator");
+    pos.Set("X", StringValue("ns3::UniformRandomVariable[Min=0.0|Max=300.0]"));
+    pos.Set("Y", StringValue("ns3::UniformRandomVariable[Min=0.0|Max=1500.0]"));
+
+    Ptr<PositionAllocator> taPositionAlloc = pos.Create()->GetObject<PositionAllocator>();
+    streamIndex += taPositionAlloc->AssignStreams(streamIndex);
+
+    std::stringstream ssSpeed;
+    ssSpeed << "ns3::UniformRandomVariable[Min=0.0|Max=" << nodeSpeed << "]";
+    std::stringstream ssPause;
+    ssPause << "ns3::ConstantRandomVariable[Constant=" << nodePause << "]";
+    //mover nodos
+    mobilityAdhoc.SetMobilityModel("ns3::RandomWaypointMobilityModel",
+                                   "Speed",
+                                   StringValue(ssSpeed.str()),
+                                   "Pause",
+                                   StringValue(ssPause.str()),
+                                   "PositionAllocator",
+                                   PointerValue(taPositionAlloc));
+    mobilityAdhoc.SetPositionAllocator(taPositionAlloc);
+    mobilityAdhoc.Install(adhocNodes1);
+    mobilityAdhoc.Install(adhocNodes2);
+    streamIndex += mobilityAdhoc.AssignStreams(adhocNodes1, streamIndex);
+
+    AodvHelper aodv;
+    OlsrHelper olsr;
+    DsdvHelper dsdv;
+    DsrHelper dsr;
+    DsrMainHelper dsrMain;
+    Ipv4ListRoutingHelper list;
+    InternetStackHelper internet;
+
+    if (m_protocolName == "OLSR")
+    {
+        list.Add(olsr, 100);
+        internet.SetRoutingHelper(list);
+        internet.Install(adhocNodes1);
+    }
+    else if (m_protocolName == "AODV")
+    {
+        list.Add(aodv, 100);
+        internet.SetRoutingHelper(list);
+        internet.Install(adhocNodes1);
+    }
+    else if (m_protocolName == "DSDV")
+    {
+        list.Add(dsdv, 100);
+        internet.SetRoutingHelper(list);
+        internet.Install(adhocNodes1);
+    }
+    else if (m_protocolName == "DSR")
+    {
+        internet.Install(adhocNodes)1;
+        dsrMain.Install(dsr, adhocNodes1);
+        if (m_flowMonitor)
+        {
+            NS_FATAL_ERROR("Error: FlowMonitor does not work with DSR. Terminating.");
+        }
+    }
+    else
+    {
+        NS_FATAL_ERROR("No such protocol:" << m_protocolName);
+    }
+
+    NS_LOG_INFO("assigning ip address");
+
+    Ipv4AddressHelper addressAdhoc;
+
+    addressAdhoc.SetBase("193.1.1.0", "255.255.255.0");
+    Ipv4InterfaceContainer adhocInterfaces1;
+    adhocInterfaces1 = addressAdhoc.Assign(adhocDevices1);
+
+    addressAdhoc.SetBase("10.1.2.0", "255.255.255.0");
+    Ipv4InterfaceContainer adhocInterfaces2;
+    adhocInterfaces2 = addressAdhoc.Assign(adhocDevices2);
+    
+
+
+}
