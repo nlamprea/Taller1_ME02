76fd8f2bd90a852aca701b82a226ec512eb9b7cf
nlamprea <nlamprea@unal.edu.co>
2025-02-10 17:54:10 -0500

    taller

--- taller1.cc
+++ taller1.cc
@@ -1,29 +1,22 @@
-#include "ns3/applications-module.h"
 #include "ns3/core-module.h"
-#include "ns3/csma-module.h"
 #include "ns3/internet-module.h"
-#include "ns3/mobility-module.h"
 #include "ns3/network-module.h"
+#include "ns3/applications-module.h"
+#include "ns3/wifi-module.h"
+#include "ns3/mobility-module.h"
 #include "ns3/point-to-point-module.h"
-#include "ns3/ssid.h"
+#include "ns3/aodv-module.h"
+#include "ns3/dsdv-module.h"
+#include "ns3/dsr-module.h"
+#include "ns3/flow-monitor-module.h"
+#include "ns3/olsr-module.h"
 #include "ns3/yans-wifi-helper.h"
-
-// Default Network Topology
-//
-//   Wifi 10.1.3.0
-//                 AP                AP
-//  *    *    *    *                 *    *    *    *
-//  |    |    |    |    10.1.1.1
-// n5   n6   n7   n0 -------------- n1   n2   n3   n4
-//                   point-to-point  |    |    |    |
-//                                   ================
-//                                     wifi 10.1.2.0
 
 using namespace ns3;
 
-NS_LOG_COMPONENT_DEFINE("WIFI_NET");
+NS_LOG_COMPONENT_DEFINE("Taller1ME02");
 
-//Funciones de callback para los traces de IPv4
+// Funciones de callback para los traces de IPv4
 static void Ipv4TxTrace(Ptr<const Packet> packet, Ptr<Ipv4> ipv4, uint32_t interface)
 {
     Ipv4Header ipHeader;
@@ -40,165 +33,238 @@ static void Ipv4RxTrace(Ptr<const Packet> packet, Ptr<Ipv4> ipv4, uint32_t inter
 }
 
 
-int main(int argc, char* argv[]){
+static std::string PrintReceivedPacket(Ptr<Socket> socket, Ptr<Packet> packet, Address senderAddress)
+{
+    std::ostringstream oss;
 
-    bool verbose = true;
-    int nodesWifi1 = 5; // Number of nodes
-    int nodesWifi2 = 5; // Number of nodes
-    double totalTime = 100.0; // Total simulation time
-    std::string rate("2048bps");
-    std::string phyMode("DsssRate11Mbps");
-    std::string tr_name("manet-routing-compare");
-    // int nodeSpeed = 20; // in m/s
-    // int nodePause = 0;  // in s
+    oss << Simulator::Now().GetSeconds() << " " << socket->GetNode()->GetId();
 
+    if (InetSocketAddress::IsMatchingType(senderAddress))
+    {
+        InetSocketAddress addr = InetSocketAddress::ConvertFrom(senderAddress);
+        oss << " received one packet from " << addr.GetIpv4();
+    }
+    else
+    {
+        oss << " received one packet!";
+    }
+    return oss.str();
+}
+
+static void ReceivePacket(Ptr<Socket> socket)
+{
+    Ptr<Packet> packet;
+    Address senderAddress;
+    while ((packet = socket->RecvFrom(senderAddress)))
+    {
+        bytesTotal += packet->GetSize();
+        packetsReceived += 1;
+        NS_LOG_UNCOND(PrintReceivedPacket(socket, packet, senderAddress));
+    }
+}
+
+static void CheckThroughput()
+{
+    double kbs = (bytesTotal * 8.0) / 1000;
+    bytesTotal = 0;
+
+    std::ofstream out(m_CSVfileName, std::ios::app);
+
+    out << (Simulator::Now()).GetSeconds() << "," << kbs << "," << packetsReceived << ","
+        << m_nSinks << "," << m_protocolName << "," << m_txp << "" << std::endl;
+
+    out.close();
+    packetsReceived = 0;
+    Simulator::Schedule(Seconds(1.0), &RoutingExperiment::CheckThroughput, this);
+}
+
+
+int main(int argc, char *argv[]) {
+
+    bool packetReceive = true;
+    bool verbose = false;
+    bool printRoutes = true;
+    std::string m_protocolName{"AODV"};    
 
     CommandLine cmd(__FILE__);
-    cmd.AddValue("nodesWifi1", "Numero de nodos de la red 1", nodesWifi1);
-    cmd.AddValue("nodesWifi2", "Numero de nodos de la red 2", nodesWifi2);
+    cmd.AddValue("packetReceive", "Ver paquetes", packetReceive);
+    cmd.AddValue("protocol", "Routing protocol (OLSR, AODV, DSDV)", m_protocolName);
     cmd.AddValue("verbose", "Tell echo applications to log if true", verbose);
-    cmd.AddValue("totalTime", "Enable pcap tracing", totalTime);
+
 
     cmd.Parse(argc, argv);
-
+    // Enable logging
     if (verbose)
     {
         LogComponentEnable("UdpEchoClientApplication", LOG_LEVEL_INFO);
         LogComponentEnable("UdpEchoServerApplication", LOG_LEVEL_INFO);
+        LogComponentEnable("Aodv    RoutingProtocol", LOG_LEVEL_LOGIC);
+    }
+    
+    if (packetReceive)
+    {
+        LogComponentEnable("Taller1ME02", LOG_LEVEL_INFO); // Habilita los logs del script
     }
 
-    
+    // Create nodes for WiFi stations and APs
+    NodeContainer wifiStaNodes1, wifiStaNodes2;
+    wifiStaNodes1.Create(2); // Two stations in first WiFi
+    wifiStaNodes2.Create(2); // Two stations in second WiFi
 
-    //create clusters
     NodeContainer wifiApNodes;
-    wifiApNodes.Create(2);
+    wifiApNodes.Create(2); // Two AP nodes
 
-    //conexion punto a punto
-    PointToPointHelper pointToPoint;
-    pointToPoint.SetDeviceAttribute("DataRate", StringValue("5Mbps"));
-    pointToPoint.SetChannelAttribute("Delay", StringValue("2ms"));
+    // Set up P2P link between APs
+    PointToPointHelper p2p;
+    p2p.SetDeviceAttribute("DataRate", StringValue("5Mbps"));
+    p2p.SetChannelAttribute("Delay", StringValue("2ms"));
 
     NetDeviceContainer p2pDevices;
-    p2pDevices = pointToPoint.Install(wifiApNodes);
+    p2pDevices = p2p.Install(wifiApNodes.Get(0), wifiApNodes.Get(1));
 
+    // Configure WiFi networks
+    WifiHelper wifi;
+    wifi.SetStandard(WIFI_STANDARD_80211b);
+    wifi.SetRemoteStationManager("ns3::AarfWifiManager");
 
-    //create nodes
-    NodeContainer wifiStaNodes1;
-    wifiStaNodes1.Create(nodesWifi1);
-    NodeContainer wifiApNode1 = wifiApNodes.Get(1);
-    NodeContainer wifiStaNodes2;
-    wifiStaNodes2.Create(nodesWifi2);
-    NodeContainer wifiApNode2 = wifiApNodes.Get(0);
-
-    YansWifiChannelHelper channel = YansWifiChannelHelper::Default();
+    YansWifiChannelHelper channel;
+    channel.SetPropagationDelay("ns3::ConstantSpeedPropagationDelayModel");
+    channel.AddPropagationLoss("ns3::FriisPropagationLossModel");
     YansWifiPhyHelper phy;
     phy.SetChannel(channel.Create());
 
+    // First WiFi network
+    Ssid ssid1 = Ssid("Network1");
     WifiMacHelper mac;
-    Ssid ssid = Ssid("ns-3-ssid");
 
-    WifiHelper wifi;
+    mac.SetType("ns3::StaWifiMac",
+                "Ssid", SsidValue(ssid1),
+                "ActiveProbing", BooleanValue(true));
+    NetDeviceContainer staDevices1 = wifi.Install(phy, mac, wifiStaNodes1);
 
+    mac.SetType("ns3::ApWifiMac", "Ssid", SsidValue(ssid1));
+    NetDeviceContainer apDevices1 = wifi.Install(phy, mac, wifiApNodes.Get(0));
 
-    NodeContainer wifiApNode;
-    wifiApNode.Add(wifiApNodes.Get(0)); // AP1
-    wifiApNode.Create(1); // AP2    
+    // Second WiFi network
+    Ssid ssid2 = Ssid("Network2");
+    mac.SetType("ns3::StaWifiMac",
+                "Ssid", SsidValue(ssid2),
+                "ActiveProbing", BooleanValue(true));
+    NetDeviceContainer staDevices2 = wifi.Install(phy, mac, wifiStaNodes2);
 
-    //NodeContainer wifiApNode2 = wifiApNodes.Get(1);
+    mac.SetType("ns3::ApWifiMac", "Ssid", SsidValue(ssid2));
+    NetDeviceContainer apDevices2 = wifi.Install(phy, mac, wifiApNodes.Get(1));
 
-
-    //contenedor de dispositivos
-    NetDeviceContainer staDevice1;
-    NetDeviceContainer staDevice2;
-    NetDeviceContainer wifiApNodes1;
-    NetDeviceContainer wifiApNodes2;
-
-    // Network 1
-    mac.SetType("ns3::StaWifiMac", "Ssid", SsidValue(ssid), "ActiveProbing", BooleanValue(false));
-    staDevice1 = wifi.Install(phy, mac, wifiStaNodes1);
-
-    mac.SetType("ns3::ApWifiMac", "Ssid", SsidValue(ssid));
-    wifiApNodes1 = wifi.Install(phy, mac, wifiApNode1);
-
-    // Network 2
-    mac.SetType("ns3::StaWifiMac", "Ssid", SsidValue(ssid), "ActiveProbing", BooleanValue(false));
-    staDevice2 = wifi.Install(phy, mac, wifiStaNodes1);
-
-    mac.SetType("ns3::ApWifiMac", "Ssid", SsidValue(ssid));
-    wifiApNodes2 = wifi.Install(phy, mac, wifiApNode2);
-
-    // movilidad de los nodos   
+    // Mobility configuration
     MobilityHelper mobility;
-
-    mobility.SetPositionAllocator("ns3::GridPositionAllocator",
-                                  "MinX",
-                                  DoubleValue(0.0),
-                                  "MinY",
-                                  DoubleValue(0.0),
-                                  "DeltaX",
-                                  DoubleValue(5.0),
-                                  "DeltaY",
-                                  DoubleValue(10.0),
-                                  "GridWidth",
-                                  UintegerValue(3),
-                                  "LayoutType",
-                                  StringValue("RowFirst"));
-
-    mobility.SetMobilityModel("ns3::RandomWalk2dMobilityModel","Bounds",RectangleValue(Rectangle(-100, 100, -100, 100)));
+    mobility.SetMobilityModel("ns3::ConstantPositionMobilityModel");
     mobility.Install(wifiStaNodes1);
     mobility.Install(wifiStaNodes2);
-    mobility.SetMobilityModel("ns3::ConstantPositionMobilityModel");
     mobility.Install(wifiApNodes);
 
-    // Internet stack
-    InternetStackHelper stack;
-    stack.Install(wifiStaNodes1);
-    stack.Install(wifiStaNodes2);
-    stack.Install(wifiApNodes);
+    // Position APs and stations
+    // AP1 at (0,0), stations nearby
+    Ptr<ConstantPositionMobilityModel> ap1Pos = wifiApNodes.Get(0)->GetObject<ConstantPositionMobilityModel>();
+    ap1Pos->SetPosition(Vector(0.0, 0.0, 0.0));
 
-    //asignacion de direcciones IP (host y mascara)
+    for (uint32_t i = 0; i < wifiStaNodes1.GetN(); ++i) {
+        Ptr<ConstantPositionMobilityModel> staPos = wifiStaNodes1.Get(i)->GetObject<ConstantPositionMobilityModel>();
+        staPos->SetPosition(Vector(5.0, 0.0, 0.0));
+    }
+
+    // AP2 at (100,0), stations nearby
+    Ptr<ConstantPositionMobilityModel> ap2Pos = wifiApNodes.Get(1)->GetObject<ConstantPositionMobilityModel>();
+    ap2Pos->SetPosition(Vector(100.0, 0.0, 0.0));
+
+    for (uint32_t i = 0; i < wifiStaNodes2.GetN(); ++i) {
+        Ptr<ConstantPositionMobilityModel> staPos = wifiStaNodes2.Get(i)->GetObject<ConstantPositionMobilityModel>();
+        staPos->SetPosition(Vector(105.0, 0.0, 0.0));
+    }
+
+    // Install Internet stack with AODV routing
+
+    AodvHelper aodv;
+    OlsrHelper olsr;
+    DsdvHelper dsdv;
+    DsrHelper dsr;
+    Ipv4ListRoutingHelper list;
+    InternetStackHelper stack;
+    if (m_protocolName == "OLSR")
+    {
+        list.Add(olsr, 100);
+        stack.SetRoutingHelper(list);
+        stack.Install(wifiStaNodes1);
+        stack.Install(wifiStaNodes2);
+        stack.Install(wifiApNodes);
+    }
+    else if (m_protocolName == "AODV")
+    {
+        list.Add(aodv, 100);
+        stack.SetRoutingHelper(list);
+        stack.Install(wifiStaNodes1);
+        stack.Install(wifiStaNodes2);
+        stack.Install(wifiApNodes);
+    }
+    else if (m_protocolName == "DSDV")
+    {
+        list.Add(dsdv, 100);
+        stack.SetRoutingHelper(list);
+        stack.Install(wifiStaNodes1);
+        stack.Install(wifiStaNodes2);
+        stack.Install(wifiApNodes);
+    }
+    else
+    {
+        NS_FATAL_ERROR("No such protocol:" << m_protocolName);
+    }
+    // AodvHelper aodv;
+    // InternetStackHelper stack;
+    // stack.SetRoutingHelper(aodv);
+    // stack.Install(wifiStaNodes1);
+    // stack.Install(wifiStaNodes2);
+    // stack.Install(wifiApNodes);
+
+    // Assign IP addresses
     Ipv4AddressHelper address;
 
+    // First WiFi network
     address.SetBase("10.1.1.0", "255.255.255.0");
-    Ipv4InterfaceContainer p2pInterfaces;
-    p2pInterfaces = address.Assign(p2pDevices);
+    Ipv4InterfaceContainer staInterfaces1 = address.Assign(staDevices1);
+    address.Assign(apDevices1);
 
-    address.SetBase("192.168.1.0", "255.255.255.0");
-    Ipv4InterfaceContainer StaInterface1;
-    StaInterface1 = address.Assign(staDevice1);
-    Ipv4InterfaceContainer ApInterface1;
-    ApInterface1 = address.Assign(wifiApNodes1);
+    // Second WiFi network
+    address.SetBase("10.1.2.0", "255.255.255.0");
+    Ipv4InterfaceContainer staInterfaces2 = address.Assign(staDevices2);
+    address.Assign(apDevices2);
 
-    address.SetBase("192.168.2.0", "255.255.255.0");
-    Ipv4InterfaceContainer StaInterface2;
-    StaInterface2 = address.Assign(staDevice2);
-    Ipv4InterfaceContainer ApInterface2;
-    ApInterface2 = address.Assign(wifiApNodes2);
+    // P2P network
+    address.SetBase("10.1.3.0", "255.255.255.252");
+    Ipv4InterfaceContainer p2pInterfaces = address.Assign(p2pDevices);
 
-    //habilitar trazas de los paquetes
+    // Applications (UDP Echo for testing)
+    UdpEchoServerHelper echoServer(9);
+    ApplicationContainer serverApps = echoServer.Install(wifiStaNodes2.Get(0));
+    serverApps.Start(Seconds(1.0));
+    serverApps.Stop(Seconds(10.0));
 
-    // UdpEchoServerHelper echoServer(9);
+    UdpEchoClientHelper echoClient(staInterfaces2.GetAddress(0), 9);
+    echoClient.SetAttribute("MaxPackets", UintegerValue(2));
+    echoClient.SetAttribute("Interval", TimeValue(Seconds(1.0)));
+    echoClient.SetAttribute("PacketSize", UintegerValue(1024));
 
-    // ApplicationContainer serverApps = echoServer.Install(wifiStaNodes1.Get(nodesWifi1 - 1));
-    // serverApps.Start(Seconds(1.0));
-    // serverApps.Stop(Seconds(10.0));
+    ApplicationContainer clientApps = echoClient.Install(wifiStaNodes1.Get(0));
+    clientApps.Start(Seconds(2.0));
+    clientApps.Stop(Seconds(10.0));
 
-    // UdpEchoClientHelper echoClient(StaInterface1.GetAddress(nodesWifi1), 9);
-    // echoClient.SetAttribute("MaxPackets", UintegerValue(1));
-    // echoClient.SetAttribute("Interval", TimeValue(Seconds(1.0)));
-    // echoClient.SetAttribute("PacketSize", UintegerValue(1024));
+    // Enable PCAP tracing
+    phy.EnablePcapAll("two_wifi_aodv");
 
-    // ApplicationContainer clientApps = echoClient.Install(wifiStaNodes2.Get(nodesWifi2 - 1));
-    // clientApps.Start(Seconds(2.0));
-    // clientApps.Stop(Seconds(10.0));
 
-    // Ipv4GlobalRoutingHelper::PopulateRoutingTables();
-
-    // Simulator::Stop(Seconds(10.0));
+    Ipv4GlobalRoutingHelper::PopulateRoutingTables();
 
     // Conectar los traces a los nodos cliente y servidor
-    Ptr<Node> clientNode = wifiStaNodes1.Get(nodesWifi1 - 1);
-    Ptr<Node> serverNode = wifiStaNodes2.Get(nodesWifi1 - 1);
+    Ptr<Node> clientNode = wifiStaNodes2.Get(0);
+    Ptr<Node> serverNode = wifiStaNodes1.Get(0);
 
     Ptr<Ipv4> clientIpv4 = clientNode->GetObject<Ipv4>();
     clientIpv4->TraceConnectWithoutContext("Tx", MakeCallback(&Ipv4TxTrace));
@@ -208,10 +274,18 @@ int main(int argc, char* argv[]){
     serverIpv4->TraceConnectWithoutContext("Tx", MakeCallback(&Ipv4TxTrace));
     serverIpv4->TraceConnectWithoutContext("Rx", MakeCallback(&Ipv4RxTrace));
 
+
+    if (printRoutes)
+     {
+         Ptr<OutputStreamWrapper> routingStream =
+             Create<OutputStreamWrapper>("/home/nicolas/ns-allinone-3.43/ns-3.43/scratch/taller.routes", std::ios::out);
+         Ipv4RoutingHelper::PrintRoutingTableAllAt(Seconds(8), routingStream);
+     }
+
+    // Run simulation
     Simulator::Stop(Seconds(10.0));
-
-
     Simulator::Run();
     Simulator::Destroy();
+
     return 0;
 }
